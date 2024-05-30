27226107a4b2591444c2dba1f4c08658261e7e12
luhsdm <luh.sdm@gmail.com>
2024-05-30 17:11:01 -0300

    criacao static root, static files

--- adminlte/settings.py
+++ adminlte/settings.py
@@ -136,6 +136,7 @@ LOGGING = {
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'adminlte/static'),
 ]
