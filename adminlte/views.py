813661c313a86f2bd9d9336d55d0e59f0c95a27a
luhsdm <luh.sdm@gmail.com>
2024-05-31 20:19:23 -0300

    ajuste na views raiz

--- adminlte/views.py
+++ adminlte/views.py
@@ -3,32 +3,30 @@ from django.contrib.auth.decorators import login_required
 
 from livro.models import Livros
 
-# Create your views here.
 def cadastro(request):
     return render(request, 'cadastro.html')
 
 def login(request):
     return render(request, 'login.html')
 
-
 def home(request):
     return render(request, 'home.html' )
 
-
 def ver_livros(request):
-    return render(request, 'ver_livros.html')
-
+    livros = Livros.objects.all()
+    return render(request, 'ver_livros.html', {'livros': livros})
 
 def ver_emprestimos(request):
     return render(request, 'ver_emprestimos.html')
-
 
 def ver_devolucao(request):
     return render(request, 'ver_devolucao.html')
 
 def buscar_livros(request):
-    search_query = request.POST.get('search_query', '')
-    livros = Livros.objects.filter(nome__contains=search_query)
-
-    return render(request, 'buscar_livros.html', {'livros': livros, 'resultado_busca': livros.count()})
-    return render(request, 'buscar_livros.html')
+    if request.method == 'POST':
+        search_query = request.POST.get('search_query')
+        livros = Livros.objects.filter(nome__contains=search_query)
+        resultado_busca = livros.count()
+        return render(request, 'buscar_livros.html', {'livros': livros, 'resultado_busca': resultado_busca})
+    else:
+        return render(request, 'buscar_livros.html')
