087f101995e110a3bf8416aea863ec0edc12f4c5
luhsdm <luh.sdm@gmail.com>
2024-05-31 14:20:24 -0300

    acrescimo na views raiz

--- adminlte/urls.py
+++ adminlte/urls.py
@@ -4,16 +4,16 @@ from adminlte import views
 from django.conf.urls.static import static
 from django.conf import settings
 from usuarios import views as usuarios_views
+from adminlte import views as adminlte_views
+from usuarios import views as usuarios_views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.login, name='login'),
-    path('home/', views.home, name='home'),
+    path('', adminlte_views.login, name='login'),
+    path('home/', adminlte_views.home, name='home'),
     path('usuarios/', include('usuarios.urls')),
     path('livro/', include('livro.urls')),
-    path('ver_livros/', views.ver_livros, name='ver_livros'),
-    path('ver_emprestimos/', views.ver_emprestimos, name='ver_emprestimos'),
-    path('valida_login/', usuarios_views.valida_login, name='valida_login'),
-
-
+    path('ver_livros/', adminlte_views.ver_livros, name='ver_livros'),
+    path('ver_emprestimos/', adminlte_views.ver_emprestimos, name='ver_emprestimos'),
+    path('valida_login/', usuarios_views.valida_login, name='valida_login'),  # Use o prefixo usuarios_ aqui
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
