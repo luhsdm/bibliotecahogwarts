574ba51be427538c8597bc13d80403ce087b57cc
luhsdm <luh.sdm@gmail.com>
2024-05-30 21:40:22 -0300

    MODIFICACAO NOME PARA BASE 2

--- adminlte/urls.py
+++ adminlte/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', views.login, name='login'),
     path('usuarios/', include('usuarios.urls')),
     path('livro/', include('livro.urls')),
     path('ver_livros/', views.ver_livros, name='ver_livros'),
