3296777e7799930837fa6ada5b4c4361c01a4373
luhsdm <luh.sdm@gmail.com>
2024-05-31 17:33:36 -0300

    retirei login required e adc buscar_livros

--- adminlte/urls.py
+++ adminlte/urls.py
@@ -3,11 +3,9 @@ from django.urls import path, include
 from adminlte import views
 from django.conf.urls.static import static
 from django.conf import settings
-from livro.views import buscar_livros, ver_emprestimos, ver_livros
 from usuarios import views as usuarios_views
 from adminlte import views as adminlte_views
 from usuarios import views as usuarios_views 
-from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,9 +13,9 @@ urlpatterns = [
     path('home/', adminlte_views.home, name='home'),
     path('usuarios/', include('usuarios.urls')),
     path('livro/', include('livro.urls')),
-    path('ver_emprestimos/', login_required(ver_emprestimos), name='ver_emprestimos'),
-    path('ver_livros/', login_required(ver_livros), name='ver_livros'),
+    path('ver_livros', views.ver_livros, name='ver_livros'),
+    path('ver_emprestimos', views.ver_emprestimos, name="ver_emprestimos"),
     path('valida_login/', usuarios_views.valida_login, name='valida_login'), 
-    path('buscar_livros/', login_required(buscar_livros), name='buscar_livros'),
-    # Use o prefixo usuarios_ aqui
+    path('buscar_livros/', views.buscar_livros, name='buscar_livros')
+   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
